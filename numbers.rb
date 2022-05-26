60b285da10517730030a0919f4aa725c6328bf4e
MarcosPalma <palma.a.marcos@gmail.com>
2022-05-26 13:13:34 -0400

    float

---
+++ numbers.rb
@@ -0,0 +1,17 @@
+#integer
+numero1=20
+numero2=30
+la_suma= numero1+numero2
+puts la_suma
+puts numero1.class
+puts numero2.class
+puts la_suma.class
+#numero tipo flotante
+num_flotante1=2.56
+num_flotante2=5.32
+otra_suma= num_flotante1 + num_flotante2
+puts num_flotante1.class
+puts num_flotante2.class
+puts otra_suma.class
+suma_final= la_suma + otra_suma
+puts suma_final.class
