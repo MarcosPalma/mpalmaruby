01cf8ce4595e05d64e1bd3440c2f33ed346a8ae5
MarcosPalma <palma.a.marcos@gmail.com>
2022-05-27 10:15:57 -0400

    trabajando con metodos numericos

---
+++ metodosnumbers.rb
@@ -0,0 +1,19 @@
+numero_entero = 1777
+#metodo numero entero siguiente o sucesor
+puts numero_entero.succ
+puts numero_siguiente=numero_entero.succ
+#metodo validar numero par o impar
+puts numero_entero.odd?
+puts numero_siguiente.odd?
+puts numero_siguiente.even?
+#conversion de entero a flotante
+puts numero_siguiente/numero_entero.to_f
+puts 800/270.to_f
+#ponerle en digitos un numero
+print 356.digits
+print 78905.digits
+#redondear un numero flotante
+puts 5609.78203.round
+puts 5609.78203.to_i
+puts 5609.78203.floor
+puts 5609.78203.ceil
